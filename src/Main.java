5b333b2a40fba96ddbf5176cc2ee8179ba6d9d14
KatOli4ka <katoli4ka87@mail.ru>
2022-10-28 21:22:10 +0500

    Деление переменной

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,8 @@ public class Main {
         System.out.println(frog);
         frog=frog*10;
         System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
 
 
     }
