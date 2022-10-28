fcf3619998f4db7103feb7e8b3e757e9e4c4505f
KatOli4ka <katoli4ka87@mail.ru>
2022-10-28 21:22:54 +0500

    Увеличение переменной

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,8 @@ public class Main {
         System.out.println(frog);
         frog=frog/3.5;
         System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
 
 
     }
